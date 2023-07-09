6ebd3fc085a11235b8659452d6d08279aba02bd9
David Dettloff <dettloffd@gmail.com>
2023-07-09 14:31:17 -0400

    Update logger for cleaner logs in AWS

--- server/src/util/Logger.ts
+++ server/src/util/Logger.ts
@@ -1,6 +1,31 @@
 import * as winston from 'winston'
 
-const { combine, timestamp, printf, colorize, simple, json } = winston.format;
+const { combine, timestamp, colorize, simple } = winston.format;
+
+const transports = [];
+
+if (process.env.NODE_ENV !== 'prod') {
+  // Add file transports only in non-production environment
+  transports.push(
+    new winston.transports.File({ filename: 'logs/server_error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/server_combined.log' }),
+  );
+}
+
+// Build the console format
+let consoleFormat = [timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), simple()];
+if (process.env.NODE_ENV !== 'prod') {
+  // Add colorize to the start of the array in non-prod environments
+  consoleFormat.unshift(colorize());
+}
+
+// Add console transport in all environments
+transports.push(
+  new winston.transports.Console({
+    level: process.env.NODE_ENV === 'prod' ? 'info' : 'silly',
+    format: combine(...consoleFormat),
+  })
+);
 
 export const logger = winston.createLogger({
   level: 'info',
@@ -10,26 +35,5 @@ export const logger = winston.createLogger({
     }),
     simple()
   ),
-  transports: [
-    new winston.transports.File({ filename: 'logs/server_error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/server_combined.log' }),
-  ],
+  transports: transports,
 });
-
-if (process.env.NODE_ENV !== 'prod') {
-    logger.add(new winston.transports.Console({
-    level: 'silly',
-      format: combine(
-        colorize(),
-        timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss',
-        }),
-        simple(),
-      ),
-    }));
-  }
-
-
-// const logFormat = printf(({ level, message, timestamp }) => {
-//     return `${timestamp} ${level}: ${message}`;
-// });
